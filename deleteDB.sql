8f2089eb72391d15aad0172ae459e66851c254ce
Jiaqi-Y <77703776+Jiaqi-Y@users.noreply.github.com>
2021-07-17 23:55:27 -0500

    Add files via upload

---
+++ deleteDB.sql
@@ -0,0 +1,12 @@
+USE world_covid19_vaccination;
+
+DROP TABLE country;
+DROP TABLE vaccine;
+DROP TABLE country_vaccine_relationship;
+DROP TABLE state;
+DROP TABLE hospital;
+DROP TABLE people;
+DROP TABLE tb_user;
+
+
+DROP DATABASE world_covid19_vaccination;
